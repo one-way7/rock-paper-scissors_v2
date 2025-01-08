0a370a23ebd7401eb07e5a4c824abdd7524b8418
Vladislav <vladislav.work98@gmail.com>
2025-01-08 19:04:37 +0100

    Create getHumanChoice function

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ function getComputerChoice() {
     const choiceNumber = Math.floor(Math.random() * 3);
     return ['rock', 'paper', 'scissors'][choiceNumber];
 }
+
+function getHumanChoice() {
+    let humanChoice = '';
+    let count = 0;
+
+    do {
+        humanChoice = prompt('What\'s your sign?').trim().toLowerCase();
+
+        switch (humanChoice) {
+            case 'rock':
+            case 'paper':
+            case 'scissors': {
+                count = 1;
+                break;
+            }
+            default: {
+                alert('Type correct input');
+            }
+        }
+    } while (count < 1);
+
+    return humanChoice;
+}
